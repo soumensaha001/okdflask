7b018a20714fe5baff7b7a32af73359036612dcb
unknown <sousaha28@gmail.com>
2020-11-23 20:01:15 +0530

    bug fix

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-# app.py
-from flask import Flask           # import flask
-app = Flask(__name__)             # create an app instance
+from flask import Flask  # From module flask import class Flask
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
-@app.route("/hi")                   # at the end point /
-def hello():                      # call method hello
-    return "Hello World!"         # which returns "hello world"
-if __name__ == "__main__":        # on running python app.py
-    app.run()                     # run the flask app
+@app.route('/')   # URL '/' to be handled by main() route handler
+def main():
+    """Say hello"""
+    return 'Hello, world!'
+
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello World! Built with a Docker file.")
+    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
